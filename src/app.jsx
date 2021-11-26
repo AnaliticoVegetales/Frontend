1a8f4a2cc07152c00efc8f58bcead32dd62f04df
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-11-25 19:29:11 -0500

    Adicion de librerias (tailwind,react-router-dom)

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState, useRef} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './index.css';
+import './App.css';
+
+
+function App() {
+  return (
+   
+      
+   <div></div>
+    
+
+  );
+}
+
+
+export default App;
+
