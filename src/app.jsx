be85364ab29bda3b86ae25379e8b48004b300d84
Ezequiel <lopezriosezequiel@gmail.com>
2021-11-25 22:52:17 -0500

    Error404

--- src/app.jsx
+++ src/app.jsx
@@ -12,7 +12,7 @@ import Index from './pages/Index';
 import Perfil from './pages/admin/Perfil';
 import Clientes from './pages/admin/Clientes';
 import Empleados from './pages/admin/Empleados';
-import Error404 from "pages/auth/Error404";
+import Error404 from "pages/Error404";
 
 
 //Styles
