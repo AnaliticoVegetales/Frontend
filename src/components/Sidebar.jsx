1d2ff414d30025fc301a0f7c3c5882ed99062e98
Ezequiel <lopezriosezequiel@gmail.com>
2021-11-26 09:58:58 -0500

    Correción Icono Pedidos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -47,7 +47,7 @@ const Sidebar = () => {
             {/* <PrivateComponent roleList={['Administrador','Lider','Estudiante']}> */} 
               <Link to='/admin'>
                 <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-300 hover:text-gray-500">
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-200"><i className="fa-shipping-fast"></i></span>
+                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-200"><i className="fas fa-shipping-fast"></i></span>
                   <span className="text-sm font-medium">Pedidos</span>
                 </a>
               </Link>
