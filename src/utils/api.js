17dd1f24ea602e46516181ca86b36b01dbe9b047
Ezequiel <lopezriosezequiel@gmail.com>
2021-11-26 12:11:05 -0500

    Autenticacion

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+
+const baseURL = 'http://localhost:5000';
+const getToken = () => {
+  return `Bearer ${localStorage.getItem('token')}`;
+};
+
 export const obtenerProductos = async (setProductos, setEjecutarConsulta) => {
   const options = { method: 'GET', url: 'http://localhost:5000/productos/' };
   await axios
@@ -13,15 +19,36 @@ export const obtenerProductos = async (setProductos, setEjecutarConsulta) => {
   setEjecutarConsulta(false);
 };
 
-export const obtenerUsuarios = async (setUsuarios, setEjecutarConsulta) => {
-  const options = { method: 'GET', url: 'http://localhost:5000/usuarios/' };
-  await axios
-    .request(options)
-    .then(function (response) {
-      setUsuarios(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-  setEjecutarConsulta(false);
+export const obtenerUsuarios = async (successCallback, errorCallback) => {
+  const options = {
+    method: 'GET',
+    url: `${baseURL}/usuarios/`,
+    headers: {
+      Authorization: getToken(),
+    },
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const obtenerDatosUsuario = async (successCallback, errorCallback) => {
+  const options = {
+    method: 'GET',
+    url: `${baseURL}/usuarios/self/`,
+    headers: {
+      Authorization: getToken(), 
+      // Enviarle el token a backend
+    },
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+
+export const editarUsuario = async (id, data, successCallback, errorCallback) => {
+  const options = {
+    method: 'PATCH',
+    url: `${baseURL}/usuarios/${id}/`,
+    headers: { 'Content-Type': 'application/json', Authorization: getToken() },
+    data,
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
 };
