dec354c4dfa9fe24a9db7b8c32deee6acf87cf5b
Duvan <duvanga@hotmail.es>
2021-11-26 11:11:43 -0500

    Tablas con backend

---
+++ src/pages/admin/Usuarios.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Table from '../../components/tableusers'
+const Clientes = () => {
+    return (
+        <div>
+            <Table/>
+        </div>
+    )
+}
+
+export default Clientes
